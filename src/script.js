775f340ad77a386979e9ed7868bf926d23c3ed5a
PxMach <moundjidmachghour58@gmail.com>
2025-04-16 23:30:11 +0100

    Suppression de code commenté dans le fichier script.js pour améliorer la lisibilité et la clarté du code.

--- src/script.js
+++ src/script.js
@@ -22,31 +22,3 @@ icons.forEach((icon) => {
       }
    });
 });
-
-// const icons = document.querySelectorAll(".icon");
-
-// icons.forEach((icon) => {
-//    icon.addEventListener("click", function () {
-//       // Sélectionner la section parente de l'icône cliquée
-//       const section = this.closest(".question-answer");
-
-//       // Sélectionner les éléments spécifiques à cette section
-//       const iconPlus = section.querySelector(".plus");
-//       const iconMinus = section.querySelector(".minus");
-//       const hiddenParagraph = section.querySelector(".hidden-paragraph");
-
-//       // Alterner l'affichage uniquement pour la section cliquée
-//       if (
-//          hiddenParagraph.style.display === "none" ||
-//          !hiddenParagraph.style.display
-//       ) {
-//          hiddenParagraph.style.display = "flex";
-//          iconPlus.style.display = "none";
-//          iconMinus.style.display = "flex";
-//       } else {
-//          hiddenParagraph.style.display = "none";
-//          iconPlus.style.display = "flex";
-//          iconMinus.style.display = "none";
-//       }
-//    });
-// });
